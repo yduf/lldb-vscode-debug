33ee13e51c327a013ec594e2a19660eb4b6bb5ba
yves <you@example.com>
2022-09-26 19:44:04 +0200

    [vscode] - launch config

---
+++ hello.cc
@@ -0,0 +1,12 @@
+// see launch.json for config
+// compile with 
+// $ g++ hello.cc -g -o hello
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    cout << "hello\n";
+}
